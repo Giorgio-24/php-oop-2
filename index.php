9edbdc0ac84c0150ee296baa0c11234bbeccb319
Giorgio Andrei <cipuandrei@libero.it>
2021-10-06 18:59:07 +0200

    exercise done

--- index.php
+++ index.php
@@ -7,19 +7,33 @@ Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello sho
 $c = new CreditCard(..);
 $user->insertCreditCard($c); */
 
+/*//? trait Premium{
+
+}
+
+trait Standard{
+
+} */
+
 class Product
 {
     protected $name;
     protected $price;
     protected $provenience;
 
-    protected function __construct($_name, $_price, $_provenience)
+    public function __construct($_name, $_price, $_provenience)
     {
         $this->name = $_name;
         $this->price = $_price;
         $this->provenience = $_provenience;
     }
+
+    public function getDetails()
+    {
+        return "Product: $this->name" . " </br> " . "Price: $this->price Euro" . " </br> " . "Provenience: $this->provenience" . " </br> ";
+    }
 };
+
 
 class User
 {
@@ -27,9 +41,9 @@ class User
     protected $last_name;
     protected $age;
     protected $nickname;
-    protected $subscription_type;
 
-    protected function __construct($_first_name, $_last_name, $_age, $_nickname, $_subscription_type)
+
+    public function __construct($_first_name, $_last_name, $_age, $_nickname, $_subscription_type)
     {
         $this->first_name = $_first_name;
         $this->last_name = $_last_name;
@@ -38,3 +52,34 @@ class User
         $this->subscription_type = $_subscription_type;
     }
 };
+
+class PremiumUser extends User
+{
+
+    public function __construct($_first_name, $_last_name, $_age, $_nickname, $_subscription_type)
+    {
+        $this->first_name = $_first_name;
+        $this->last_name = $_last_name;
+        $this->age = $_age;
+        $this->nickname = $_nickname;
+        $this->subscription_type = $_subscription_type;
+    }
+
+    public function getDiscount()
+    {
+        if ($this->subscription_type == 'premium') {
+            return "</br> Congratulazioni $this->nickname! Essendo un utente $this->subscription_type hai diritto ad uno sconto del 30%.";
+        }
+    }
+}
+
+$pen = new Product('Pen', 1, 'Made in Italy');
+$iphone = new Product('Iphone', 1200, 'Made in China');
+
+$user_1 = new PremiumUser('Giova', 'Bordi', 18, 'giovannone_8', 'premium');
+$user_2 = new User('Alessio', 'Reberi', 18, 'alereberi00', 'premium');
+
+echo ($pen->getDetails());
+echo ($iphone->getDetails());
+echo ($user_1)->getDiscount();
+var_dump($user_2);
