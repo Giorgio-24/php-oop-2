c0489bdde96b28549d5d73f3d1584b596c5d1987
Giorgio Andrei <cipuandrei@libero.it>
2021-10-06 16:20:56 +0200

    class product added

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
-/* Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
-Strutturare le classi gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+/* Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio,
+ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping. Strutturare le classi
+gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno
+diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti. 
 Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito...
 $c = new CreditCard(..);
 $user->insertCreditCard($c); */
+
+class Product
+{
+    protected $name;
+    protected $price;
+    protected $provenience;
+
+    protected function __construct($_name, $_price, $_provenience)
+    {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->provenience = $_provenience;
+    }
+};
